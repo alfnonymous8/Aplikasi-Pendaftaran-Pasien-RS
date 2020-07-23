a8355d7b581b5155f9917e6a8e9e31c63b5aebb2
alfnonymous8 <alfnonymous8@gmail.com>
2020-07-23 15:22:49 +0700

    SQL Dump

---
+++ bpjs.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 23, 2020 at 10:20 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bpjs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrian`
+--
+
+CREATE TABLE `antrian` (
+  `id` int(255) NOT NULL,
+  `spesialis` varchar(200) NOT NULL,
+  `tanggal` varchar(200) NOT NULL,
+  `no_antri` int(255) NOT NULL,
+  `nrm` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pengguna`
+--
+
+CREATE TABLE `data_pengguna` (
+  `id` int(10) NOT NULL,
+  `nrm` varchar(9) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `jenis` varchar(15) NOT NULL,
+  `tanggal` date NOT NULL,
+  `ponsel` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `sandi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konsultasi`
+--
+
+CREATE TABLE `konsultasi` (
+  `id` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `poli_tujuan` varchar(200) NOT NULL,
+  `keluhan` varchar(2048) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nrm`
+--
+
+CREATE TABLE `nrm` (
+  `id` int(10) NOT NULL,
+  `id_nrm` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nrm`
+--
+
+INSERT INTO `nrm` (`id`, `id_nrm`) VALUES
+(1, 'L87654321'),
+(2, 'P87654321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `antrian`
+--
+ALTER TABLE `antrian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `data_pengguna`
+--
+ALTER TABLE `data_pengguna`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `konsultasi`
+--
+ALTER TABLE `konsultasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nrm`
+--
+ALTER TABLE `nrm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `antrian`
+--
+ALTER TABLE `antrian`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT for table `data_pengguna`
+--
+ALTER TABLE `data_pengguna`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `konsultasi`
+--
+ALTER TABLE `konsultasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `nrm`
+--
+ALTER TABLE `nrm`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
